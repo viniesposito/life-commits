b683fb6bd4ee00707c45819a63804125bbed055e
viniesposito <viniesposito@gmail.com>
2024-02-02 16:22:22 -0500

    convex and clerk set up

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import NewButton from "./new-button";
 
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-slate-500 h-full w-[60px] p-3 flex-col space-y-2">
-      <NewButton iconType={"plus"} />
+      <div className="flex items-center justify-center opacity-60 hover:opacity-100 transition">
+        <UserButton />
+      </div>
       <NewButton iconType={"settings"} />
     </aside>
   );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://up-doberman-29.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
