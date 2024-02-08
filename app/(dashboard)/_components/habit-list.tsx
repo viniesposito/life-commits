f1320df980fe3a355e7cec49046c05e5f1ba7e46
viniesposito <viniesposito@gmail.com>
2024-02-08 13:56:51 -0500

    cleaning up habit list

--- app/(dashboard)/_components/habit-list.tsx
+++ app/(dashboard)/_components/habit-list.tsx
@@ -1,24 +1,10 @@
 "use client";
-import { TileGrid } from "@/components/TileGrid";
-import mockData from "@/data/habit_tracker_mock_data.json";
 import { NoHabits } from "./no-habits";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useUser } from "@clerk/nextjs";
 
-interface DayData {
-  date: string;
-  count: number;
-}
-
-interface HabitData {
-  habit: string;
-  data: DayData[];
-}
-
 export const HabbitList = () => {
-  //   const habits = Object.keys(Object.assign({}, ...mockData));
-  //   habits.splice(habits.indexOf("date"), 1);
   const { isSignedIn, user, isLoaded } = useUser();
   const authorId = user!.id;
   const habits = useQuery(api.habits.get, { authorId });
@@ -30,13 +16,6 @@ export const HabbitList = () => {
   if (!habits?.length) {
     return <NoHabits />;
   }
-
-  // const transformDataForHabit = (data: any[], habit: string) => {
-  //   return data.map((day) => ({
-  //     date: day.date,
-  //     count: day[habit],
-  //   }));
-  // };
 
   return <div>{JSON.stringify(habits)}</div>;
 };
