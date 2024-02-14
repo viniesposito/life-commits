b5d54e02a12c377875c6a58bbb90ac14d87b996d
viniesposito <viniesposito@gmail.com>
2024-02-13 19:12:32 -0500

    got rid of navbar, header styling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -7,7 +7,6 @@ const DashBoardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <>
       <main className="h-full flex justify-center bg-white">
-        <Sidebar />
         <div className="pl-[60px] w-1/2 sm:w-3/4 max-w-4xl">{children}</div>
       </main>
     </>
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,18 +1,34 @@
 import localFont from "next/font/local";
 import { HabbitList } from "./_components/habit-list";
+import { UserButton } from "@clerk/nextjs";
 
 const workbench = localFont({ src: "./Workbench.ttf", display: "swap" });
 
 const MyPage = () => {
   return (
-    <div className="h-full flex flex-col items-center justify-center text-center ">
-      <div>
-        <h1
-          className={`${workbench.className} text-[72px] my-4 bg-gradient-to-r from-green-950 via-green-200 to-green-500 inline-block text-transparent bg-clip-text`}
-        >
-          life commits
-        </h1>
-        <p className="my-4 text-slate-600">
+    <header className="h-full flex flex-col items-center justify-center text-center pt-10">
+      <div className="bg-stone-500 pb-6 rounded px-2">
+        <div className="flex flex-row">
+          <div className=" bg-white border-4 border-stone-600 rounded w-fit -mt-8">
+            <h1
+              className={`${workbench.className} text-[72px] bg-gradient-to-r from-green-950 via-green-200 to-green-500 inline-block text-transparent bg-clip-text`}
+            >
+              life commits
+            </h1>
+          </div>
+          <div className="grow" />
+          <div className="pt-1">
+            <UserButton
+              afterSignOutUrl="/"
+              appearance={{
+                elements: {
+                  avatarBox: "h-[64px] w-[64px]",
+                },
+              }}
+            />
+          </div>
+        </div>
+        <p className="text-slate-50 pt-4">
           Track what's meaningful to you as easy as you track your GitHub
           commits. &#128513;{" "}
         </p>
@@ -20,7 +36,7 @@ const MyPage = () => {
       <div className="h-full">
         <HabbitList />
       </div>
-    </div>
+    </header>
   );
 };
 
